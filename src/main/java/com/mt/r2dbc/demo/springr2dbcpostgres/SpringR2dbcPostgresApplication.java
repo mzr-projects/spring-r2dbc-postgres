be41bb0a1d7fd54e73a54d2dfca780440e5cbf32
Maziar Taghipour <taghipour.maziar@yahoo.com>
2024-08-21 23:37:11 -0700

    Initial commit

---
+++ src/main/java/com/mt/r2dbc/demo/springr2dbcpostgres/SpringR2dbcPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.mt.r2dbc.demo.springr2dbcpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringR2dbcPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringR2dbcPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mt/r2dbc/demo/springr2dbcpostgres/SpringR2dbcPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.r2dbc.demo.springr2dbcpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringR2dbcPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
